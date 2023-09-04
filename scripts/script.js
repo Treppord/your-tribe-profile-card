1cd49add1b4f4a50a9a6f9399c7d822b2bfe0f0d
Treppord <droppertjasper@gmail.com>
2023-09-04 12:37:25 +0200

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button');
+const article = document.querySelector('article');
+const header = document.querySelector('header');
 
-function expand () {
-  document.body.classList.toggle('expand')
+expandButton.addEventListener('click', expand);
+
+function expand() {
+  document.body.classList.toggle('expand');
+  
+  if (document.body.classList.contains('expand')) {
+    // Calculate the scroll position to include the header
+    const scrollPosition = header.offsetTop - 20; // You can adjust the offset as needed
+    
+    // Scroll to the calculated position
+    window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+    
+    // Add a class to the <dl> element for the expanded state
+    document.querySelector('dl').classList.add('expanded');
+  } else {
+    // Remove the class when the button is pressed again to collapse
+    document.querySelector('dl').classList.remove('expanded');
+  }
 }
