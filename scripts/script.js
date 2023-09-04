5e4c805cd1286838cdc996dbb726c98dd08fdc5e
Treppord <droppertjasper@gmail.com>
2023-09-04 21:20:07 +0200

    New Site

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,22 @@
-const expandButton = document.querySelector('header button');
-const article = document.querySelector('article');
-const header = document.querySelector('header');
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("click", function (event) {
+      const clickEffect = document.createElement("div");
+      clickEffect.className = "click-effect";
+      document.body.appendChild(clickEffect);
 
-expandButton.addEventListener('click', expand);
+      const x = event.clientX;
+      const y = event.clientY;
 
-function expand() {
-  document.body.classList.toggle('expand');
-  
-  if (document.body.classList.contains('expand')) {
-    // Calculate the scroll position to include the header
-    const scrollPosition = header.offsetTop - 20; // You can adjust the offset as needed
-    
-    // Scroll to the calculated position
-    window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
-    
-    // Add a class to the <dl> element for the expanded state
-    document.querySelector('dl').classList.add('expanded');
-  } else {
-    // Remove the class when the button is pressed again to collapse
-    document.querySelector('dl').classList.remove('expanded');
-  }
-}
+      // Adjust the position to be more to the upper right
+      clickEffect.style.left = x - 10 + "px";
+      clickEffect.style.top = y - 10 + "px";
+      clickEffect.style.transform = "scale(1)"; // Grow the circle
+
+      setTimeout(() => {
+          clickEffect.style.transform = "scale(0)"; // Shrink and disappear
+          clickEffect.addEventListener("transitionend", () => {
+              document.body.removeChild(clickEffect); // Remove the element after it disappears
+          });
+      }, 50);
+  });
+});
